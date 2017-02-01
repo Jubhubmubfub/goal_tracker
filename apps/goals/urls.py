748388174128dc2db1ff41d5508a30a40e28ba40
Marcus Sakoda <Jubhub@yahool.com>
2017-02-01 10:37:14 -0800

    added more html pages

--- apps/goals/urls.py
+++ apps/goals/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^dashboard$', views.dashboard),
+    url(r'^new_goal$', views.new_goal),
+    url(r'^goal$', views.goal),
 ]
--- apps/goals/views.py
+++ apps/goals/views.py
@@ -6,3 +6,17 @@ def index(request):
 
 def dashboard(request):
     return render(request,'goals/dashboard.html')
+
+def new_goal(request):
+    numbers = range(1,32)
+    numbers2 = range(1,11)
+    times = ['hours','days','weeks','months','years']
+    context = {
+        'numbers':numbers,
+        'numbers2':numbers2,
+        'times': times
+    }
+    return render(request,'goals/new_goal.html',context)
+
+def goal(request):
+    return render(request, 'goals/goal.html')
